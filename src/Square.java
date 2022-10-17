385826520e1d91ccb97f47b6cf0ed7edfa32a1b3
Emily Tang <emilytang2002@gmail.com>
2022-10-17 17:09:47 -0400

    created Multiplier, WordMultiplier, and LetterMultiplier

--- src/Square.java
+++ src/Square.java
@@ -1,3 +1,53 @@
+/**
+ * A Square in the game of Scrabble. A Letter can be placed on a Square, and a Square can have a Multiplier.
+ */
 public class Square {
-    // heheheheh
+
+    /** A Letter that is placed on the Square. */
+    private Letter letter;
+
+    /** A Letter/Word Multiplier. */
+    private Multiplier multiplier;
+
+    /**
+     * Creates a Square.
+     */
+    public Square() {
+
+    }
+
+    /**
+     * Creates a Premium Letter/Word Square with the specified multiplier.
+     * @param multiplier A Multiplier to add to the Square.
+     */
+    public Square(Multiplier multiplier) {
+        this.multiplier = multiplier;
+    }
+
+    /**
+     * @return The Letter that is on the Square.
+     */
+    public Letter getLetter() {
+        return letter;
+    }
+
+    /**
+     * @param letter A Letter to be placed on the Square.
+     * @return True, if the Letter was placed on the Square successfully. False, if not.
+     */
+    public boolean setLetter(Letter letter) {
+        if (letter != null) {
+            this.letter = letter;
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * @return True, if the Square is a Premium Square. False, if not.
+     */
+    public boolean isPremiumSquare() {
+        return multiplier != null;
+    }
+
 }
