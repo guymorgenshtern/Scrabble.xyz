1f3eb95ea6f38a5ee0564e033a7d565b1cbccf77
franciscodegrano <franciscodgrano@gmail.com>
2022-10-18 21:05:14 -0400

    Board Class

---
+++ src/Board.java
@@ -0,0 +1,20 @@
+/*
+    -- Notes -- Francisco De Grano
+
+        - Decide on Data Structure to use -> Hashmap + Hashset? https://www.w3schools.com/java/java_hashset.asp
+        - Board is 15 x 15
+
+
+        MULTIPLIERS
+        - 2 Word Multipliers Separate from Board.java -> doubleWord, tripleWord
+        - 2 Letter Multipliers Separate from Board.java -> doubleLetter, tripleLetter
+
+        - BLANK TILES           -> M3
+        - PREMIUM SQUARES       -> M3
+
+ */
+
+public class Board {
+
+
+}
