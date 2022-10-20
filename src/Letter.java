fe3a5dccfb02365cf787e6eaf1c7f669ac3c25cc
alexh <alexanderhum@cmail.carleton.ca>
2022-10-20 14:04:36 -0400

    created HashMap, bagSize, removeLetter, and addLetter

--- src/Letter.java
+++ src/Letter.java
@@ -1,2 +1,14 @@
+
 public class Letter {
+    private String letter;
+    private int score;
+
+    public Letter() {
+
+    }
+
+    public Letter(String letter, int score) {
+        this.letter = letter;
+        this.score = score;
+    }
 }
--- src/LetterBag.java
+++ src/LetterBag.java
@@ -1,3 +1,29 @@
-public class LetterBag {
-    //test
+import java.util.HashMap;
+
+public class LetterBag extends Letter {
+    //created a HashMap object called m
+    private HashMap<Letter, Integer> letterMap = new HashMap<>();
+    private int size = 0;
+    public LetterBag() {
+
+    }
+    public void addLetter(Letter letter, int quantity) {
+        letterMap.put(letter, quantity);
+    }
+    // maybe change return type depending on how we do game class
+    public void removeLetter(Letter letter, int quantity) {
+        if(letterMap.get(letter) > 1) {
+            letterMap.put(letter, letterMap.get(letter) - 1);
+        }
+        else {
+            System.out.println("There are no letters in the bag\n");
+        }
+    }
+
+    public int bagSize() {
+        for(Letter k : letterMap.keySet()) {
+            size++;
+        }
+        return size;
+    }
 }
