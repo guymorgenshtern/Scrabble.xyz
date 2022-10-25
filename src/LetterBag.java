f3b46a008d219766ce7871482680492f509bc9db
alexh <alexanderhum@cmail.carleton.ca>
2022-10-25 19:00:33 -0400

    added javadocs

--- src/LetterBag.java
+++ src/LetterBag.java
@@ -1,19 +1,41 @@
 import java.util.HashMap;
 
+/**
+ * Letter Bag that will hold letters and the quantity
+ * @author Alexander Hum 101180821
+ * @edited Guy Morgenshtern 101151430
+ */
 public class LetterBag  {
+    /** A HashMap for the letter map */
     private HashMap<String, Integer> letterMap;
+    /** size of the bag */
     private int size;
-
+    /**
+     * Creates a LetterBag.
+     * @author Alexander Hum 101180821
+     * @edited Guy Morgenshtern 101151430
+     */
     public LetterBag() {
         size = 0;
         letterMap = new HashMap<>();
     }
-
+    /**
+     * Adds a letter to letter map
+     * @param letter a string representing a letter
+     * @param quantity an integer representing the quantity of letters
+     * @author Alexander Hum 101180821
+     * @edited Guy Morgenshtern 101151430
+     */
     public void addLetter(String letter, int quantity) {
         letterMap.put(letter, quantity);
         this.size += quantity;
     }
-    // maybe change return type depending on how we do game class
+    /**
+     * removes a letter from the letter map
+     * @param letter a string representing a letter
+     * @author Alexander Hum 101180821
+     * @edited Guy Morgenshtern 101151430
+     */
     public void removeLetter(String letter) {
         letter = letter.toUpperCase();
         if (this.size > 0) {
@@ -25,7 +47,12 @@ public class LetterBag  {
             this.size--;
         }
     }
-
+    /**
+     * gets a random letter from the letter map
+     * @return the letter as a string
+     * @author Alexander Hum 101180821
+     * @edited Guy Morgenshtern 101151430
+     */
     public String getRandomLetter() {
          int random = (int) ((Math.random() * (this.size)));
          String[] availableLetters = new String[this.size];
@@ -41,7 +68,11 @@ public class LetterBag  {
          }
          return null;
     }
-
+    /**
+     * return bag size
+     * @return int for the size of the bag
+     * @author Alexander Hum 101180821
+     */
     public int bagSize() {
         return size;
     }
--- src/com/zetcode/Library.java
+++ src/com/zetcode/Library.java
@@ -1,32 +1,43 @@
 package com.zetcode;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.util.ArrayList;
 
+/**
+ * Has all allowable words for the validation checking
+ * @author Alexander Hum 101180821
+ * @edited Guy Morgenshtern 101151430
+ */
 public class Library {
-    private ArrayList<String> wordMap;
+    /** An array list for the words */
+    private ArrayList<String> acceptableWords;
 
+    /**
+     * Creates a Library with all acceptable words
+     * @throws IOException
+     * @author Alexander Hum 101180821
+     */
     public Library() throws IOException {
-        wordMap = new ArrayList<>();
+        acceptableWords = new ArrayList<>();
 
         URL url = new URL("https://www.mit.edu/~ecprice/wordlist.10000");
         BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()));
 
-        String line;
+        String word;
 
-        while ((line = br.readLine()) != null) {
-            wordMap.add(line);
+        while ((word = br.readLine()) != null) {
+            acceptableWords.add(word);
         }
     }
 
+    /**
+     * checks if the word is valid
+     * @param word String representing the word
+     * @return True if the word is valid
+     */
     public boolean isValidWord(String word) {
-        return this.wordMap.contains(word);
+        return this.acceptableWords.contains(word);
     }
-
-//    public static void main(String[] args) throws IOException{
-//        words();
-//    }
 }
