405859fd6f8ec24143000a966a4d937ba3d49a7f
franciscodegrano <franciscodgrano@gmail.com>
2022-11-08 01:31:08 -0500

    Added Code for Formatting

--- src/BoardView.java
+++ src/BoardView.java
@@ -6,10 +6,11 @@
 
         NOTE: For the time being the board will not be functional as the logic will be dealt with soon.
  */
-
+import java.awt.Color;
 import javax.swing.*;
 import java.awt.*;
-import java.swing.JPanel;
+import java.awt.event.ActionEvent;
+import javax.swing.JPanel;
 
 /*
     @author Francisco De Grano
@@ -23,6 +24,7 @@ public class BoardView extends javax.swing.JFrame {
         // Add initialize parts after initial window prompt Emily Tang init Frame?
     }
 
+    @SuppressWarnings("unchecked") // Added this line for now cause things got messy :)
     private void initComponents(){
 
         controlPanel = new javax.swing.JPanel();
@@ -34,6 +36,7 @@ public class BoardView extends javax.swing.JFrame {
         letterRack5 = new javax.swing.JButton();
         letterRack6 = new javax.swing.JButton();
         letterRack7 = new javax.swing.JButton();
+        endTurnButton = new javax.swing.JButton();
 
         player1Label = new javax.swing.JLabel();
         player1ScoreLabel = new javax.swing.JLabel();
@@ -41,11 +44,11 @@ public class BoardView extends javax.swing.JFrame {
         player2Label = new javax.swing.JLabel();
         player2ScoreLabel = new javax.swing.JLabel();
 
-        endTurnButton = new javax.swing.JButton();
+        directionalLabel = new javax.swing.JLabel();
 
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setAlwaysOnTop(true);
-        setMinimumSize(new java.awt.Dimension(650, 800));
+        setMinimumSize(new java.awt.Dimension(800, 800));
 
         setResizable(false);
 
@@ -54,10 +57,175 @@ public class BoardView extends javax.swing.JFrame {
         controlPanel.setPreferredSize(new java.awt.Dimension(616, 151));
 
         startGameButton.setText("Start New Game");
-        startGameButton.addActionListener
+        startGameButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                startGameButtonActionPerformed(evt);
+            }
+        });
 
+        letterRack1.setPreferredSize(new java.awt.Dimension(80, 62));
+        letterRack1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                letterRack1ActionPerformed(evt);
+            }
+        });
+
+        letterRack2.setPreferredSize(new java.awt.Dimension(80, 62));
+        letterRack2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                letterRack2ActionPerformed(evt);
+            }
+        });
+
+        letterRack3.setPreferredSize(new java.awt.Dimension(80, 62));
+        letterRack3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                letterRack3ActionPerformed(evt);
+            }
+        });
+
+        letterRack4.setPreferredSize(new java.awt.Dimension(80, 62));
+        letterRack4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                letterRack4ActionPerformed(evt);
+            }
+        });
+
+        letterRack5.setPreferredSize(new java.awt.Dimension(80, 62));
+        letterRack5.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                letterRack5ActionPerformed(evt);
+            }
+        });
+
+        letterRack6.setPreferredSize(new java.awt.Dimension(80, 62));
+        letterRack6.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                letterRack6ActionPerformed(evt);
+            }
+        });
+
+        letterRack7.setPreferredSize(new java.awt.Dimension(80, 62));
+        letterRack7.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                letterRack7ActionPerformed(evt);
+            }
+        });
+
+        player1Label.setText("Player 1 Score: ");
+        player1ScoreLabel.setText("0");
+
+        player2Label.setText("Player 2 Score: ");
+        player2ScoreLabel.setText("0");
+
+        directionalLabel.setText("Select where you want to place your letter");
+
+        javax.swing.GroupLayout controlPanelLayout = new javax.swing.GroupLayout(controlPanel);
+        controlPanel.setLayout(controlPanelLayout);
+        controlPanelLayout.setHorizontalGroup(
+                controlPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGroup(controlPanelLayout.createSequentialGroup()
+                                .addGroup(controlPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                        .addGroup(controlPanelLayout.createSequentialGroup()
+                                                .addContainerGap()
+                                                .addGroup(controlPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                                        .addComponent(startGameButton)
+                                                        .addComponent(endTurnButton))
+                                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                                .addGroup(controlPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                                        .addComponent(player1Label)
+                                                        .addComponent(player2Label))
+                                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                                .addGroup(controlPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                                        .addComponent(player2ScoreLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 20, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                                        .addComponent(player1ScoreLabel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                                        .addGroup(controlPanelLayout.createSequentialGroup()
+                                                .addGroup(controlPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                                        .addGroup(controlPanelLayout.createSequentialGroup()
+                                                                .addContainerGap()
+                                                                .addComponent(letterRack1, javax.swing.GroupLayout.PREFERRED_SIZE, 80, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                                                .addComponent(letterRack2, javax.swing.GroupLayout.PREFERRED_SIZE, 80, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                                                .addComponent(letterRack3, javax.swing.GroupLayout.PREFERRED_SIZE, 80, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                                                .addComponent(letterRack4, javax.swing.GroupLayout.PREFERRED_SIZE, 80, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                                                .addComponent(letterRack5, javax.swing.GroupLayout.PREFERRED_SIZE, 80, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                                                .addComponent(letterRack6, javax.swing.GroupLayout.PREFERRED_SIZE, 80, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                                                .addComponent(letterRack7, javax.swing.GroupLayout.PREFERRED_SIZE, 80, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                                        .addGroup(controlPanelLayout.createSequentialGroup()
+                                                                .addGap(12, 12, 12)
+                                                                .addComponent(directionalLabel)))
+                                                .addGap(0, 0, Short.MAX_VALUE)))
+                                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+        controlPanelLayout.setVerticalGroup(
+                controlPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGroup(controlPanelLayout.createSequentialGroup()
+                                .addGroup(controlPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                        .addGroup(controlPanelLayout.createSequentialGroup()
+                                                .addGap(6, 6, 6)
+                                                .addGroup(controlPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                                                        .addComponent(startGameButton)
+                                                        .addComponent(player1Label)
+                                                        .addComponent(player1ScoreLabel)))
+                                        .addGroup(controlPanelLayout.createSequentialGroup()
+                                                .addContainerGap()
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addGroup(controlPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                                        .addComponent(player2Label)
+                                        .addComponent(player2ScoreLabel)
+                                        .addComponent(endTurnButton))
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addComponent(directionalLabel)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addGroup(controlPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                        .addComponent(letterRack4, javax.swing.GroupLayout.PREFERRED_SIZE, 62, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                        .addGroup(controlPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                                .addComponent(letterRack1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 62, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                                .addComponent(letterRack2, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 62, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                                .addComponent(letterRack3, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 62, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                                .addComponent(letterRack4, javax.swing.GroupLayout.PREFERRED_SIZE, 62, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                                .addComponent(letterRack5, javax.swing.GroupLayout.PREFERRED_SIZE, 62, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                        .addComponent(letterRack7, javax.swing.GroupLayout.PREFERRED_SIZE, 62, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                .addGap(0, 0, 0))
+        );
+    }
+
+    private void letterRack7ActionPerformed(ActionEvent evt) {
+    }
+
+    private void letterRack6ActionPerformed(ActionEvent evt){
 
     }
+
+    private void letterRack5ActionPerformed(ActionEvent evt) {
+    }
+
+    private void letterRack4ActionPerformed(ActionEvent evt) {
+    }
+
+    private void letterRack3ActionPerformed(ActionEvent evt) {
+    }
+
+    private void letterRack2ActionPerformed(ActionEvent evt) {
+    }
+
+    private void letterRack1ActionPerformed(ActionEvent evt) {
+    }
+
+    private void startGameButtonActionPerformed(ActionEvent evt) {
+    }
+
+    public void initBoardComponents(Board gameBoard){
+
+    }
+
+
 }
 
 
