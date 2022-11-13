7f11c09fecf09987e5a562cde68ee02c6f0f4f93
franciscodegrano <franciscodgrano@gmail.com>
2022-11-13 14:16:54 -0500

    Created ScrabbleView

---
+++ src/ScrabbleView.java
@@ -0,0 +1,11 @@
+/*
+Francisco De Grano
+
+    ScrabbleView Interface
+
+    Following MVC the following is an interface and holds the property of a
+    "View" updating only.
+ */
+public interface ScrabbleView {
+    public void update(Game game); // Need to check if this is being updated properly
+}
