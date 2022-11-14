447eb89c4450298aa1a02ba60c5042726651501c
franciscodegrano <franciscodgrano@gmail.com>
2022-11-13 19:54:56 -0500

    Comments for ScrabbleController

--- src/ScrabbleController.java
+++ src/ScrabbleController.java
@@ -1,4 +1,23 @@
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
-public class ScrabbleController {
+/*
+    This class should be used to place model based methods related to the scrabble game.
+    ie:
 
+    - X Words Aligned
+    - Y Words Aligned
+    - Valid Word
+    - Calculate Scores
+    - Revert user selection
+
+    etc, etc
+ */
+
+public class ScrabbleController implements ActionListener {
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
 }
