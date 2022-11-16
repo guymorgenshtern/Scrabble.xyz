cdd6f152b736fa4314ac485b485d535b40da6688
Emily Tang <emilytang2002@gmail.com>
2022-11-16 01:59:35 -0500

    added dropdown menu to InitController

--- src/ScrabbleEvent.java
+++ src/ScrabbleEvent.java
@@ -1,32 +1,32 @@
-import java.util.EventObject;
-
-public class ScrabbleEvent extends EventObject {
-    private Player currentPlayer;
-    private Game.Status status;
-    private Board board;
-    private int coords[][];
-
-    public ScrabbleEvent(Game game, int coords[][], Player currentPlayer, Board board, Game.Status status) {
-        super(game);
-        this.status = status;
-        this.currentPlayer = currentPlayer;
-        this.board = board;
-        this.coords = coords;
-    }
-
-    public Board getBoard() {
-        return board;
-    }
-
-    public int[][] getCoords() {
-        return coords;
-    }
-
-    public Player getCurrentPlayer() {
-        return currentPlayer;
-    }
-
-    public Game.Status getStatus() {
-        return status;
-    }
-}
+//import java.util.EventObject;
+//
+//public class ScrabbleEvent extends EventObject {
+//    private Player currentPlayer;
+//    private Game.Status status;
+//    private Board board;
+//    private int coords[][];
+//
+//    public ScrabbleEvent(Game game, int coords[][], Player currentPlayer, Board board, Game.Status status) {
+//        super(game);
+//        this.status = status;
+//        this.currentPlayer = currentPlayer;
+//        this.board = board;
+//        this.coords = coords;
+//    }
+//
+//    public Board getBoard() {
+//        return board;
+//    }
+//
+//    public int[][] getCoords() {
+//        return coords;
+//    }
+//
+//    public Player getCurrentPlayer() {
+//        return currentPlayer;
+//    }
+//
+//    public Game.Status getStatus() {
+//        return status;
+//    }
+//}
