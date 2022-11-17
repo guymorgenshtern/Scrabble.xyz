ac1e79bdc7a4507f0e9fcbb9d5c412286372a21b
Emily Tang <emilytang2002@gmail.com>
2022-11-16 23:56:41 -0500

    created BotPlayer

---
+++ src/BotPlayer.java
@@ -0,0 +1,14 @@
+/**
+ * A BotPlayer in the game of Scrabble.
+ */
+public class BotPlayer extends Player {
+
+    /**
+     * Creates a BotPlayer with the specified name. BotPlayer starts off with an empty hand and a score of zero.
+     * @param name A String representing the name of the BotPlayer.
+     * @author Emily Tang 101192604
+     */
+    public BotPlayer(String name) {
+        super(name);
+    }
+}
