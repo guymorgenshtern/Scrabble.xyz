37f403bcae3cfa42afdec7a7f1cc62c3e1acaa18
franciscodegrano <franciscodgrano@gmail.com>
2022-11-22 06:15:28 -0500

    Final Grouping Prepped for Method Extract

--- src/BoardPanel.java
+++ src/BoardPanel.java
@@ -100,14 +100,6 @@ public class BoardPanel extends JPanel implements ScrabbleView {
          */
 
         //  Triple Word Red Extract Method
-
-        //  Double Word Red Extract Method
-
-        //  Triple Letter Blue Extract Method
-
-        //  Double Letter Blue Extract Method
-
-        // 0 th Row Grouping
         for (int i = 0; i <= 14; i += 7) {
             buttons[0][i].setBackground(TWRed);
             buttons[0][i].setForeground(Color.WHITE);
@@ -116,15 +108,23 @@ public class BoardPanel extends JPanel implements ScrabbleView {
             buttons[0][i].setBorderPainted(false);
         }
 
-        for (int i : new int[]{3, 11}) {
-            buttons[0][i].setBackground(DLBlue);
-            buttons[0][i].setForeground(Color.WHITE);
-            buttons[0][i].setText("DL");
-            buttons[0][i].setOpaque(true);
-            buttons[0][i].setBorderPainted(false);
+        for (int j : new int[]{0, 14}) {
+            buttons[7][j].setBackground(TWRed);
+            buttons[7][j].setForeground(Color.WHITE);
+            buttons[7][j].setText("TW");
+            buttons[7][j].setOpaque(true);
+            buttons[7][j].setBorderPainted(false);
         }
 
-        // 1 st Row Grouping
+        for (int i = 0; i <= 14; i += 7) {
+            buttons[14][i].setBackground(TWRed);
+            buttons[14][i].setForeground(Color.WHITE);
+            buttons[14][i].setText("TW");
+            buttons[14][i].setOpaque(true);
+            buttons[14][i].setBorderPainted(false);
+        }
+
+        //  Double Word Red Extract Method
         for (int i : new int[]{1, 13}) {
             buttons[1][i].setBackground(DWRed);
             buttons[1][i].setForeground(Color.WHITE);
@@ -133,16 +133,6 @@ public class BoardPanel extends JPanel implements ScrabbleView {
             buttons[1][i].setBorderPainted(false);
         }
 
-        for (int i : new int[]{5, 9}) {
-            buttons[1][i].setBackground(TLBlue);
-            buttons[1][i].setForeground(Color.WHITE);
-            buttons[1][i].setText("TL");
-            buttons[1][i].setOpaque(true);
-            buttons[1][i].setBorderPainted(false);
-        }
-
-
-        // 2 nd Row Grouping
         for (int i : new int[]{2, 12}) {
             buttons[2][i].setBackground(DWRed);
             buttons[2][i].setForeground(Color.WHITE);
@@ -151,15 +141,6 @@ public class BoardPanel extends JPanel implements ScrabbleView {
             buttons[2][i].setBorderPainted(false);
         }
 
-        for (int i : new int[]{6, 8}) {
-            buttons[2][i].setBackground(DLBlue);
-            buttons[2][i].setForeground(Color.WHITE);
-            buttons[2][i].setText("DL");
-            buttons[2][i].setOpaque(true);
-            buttons[2][i].setBorderPainted(false);
-        }
-
-        // 3 rd Row Grouping
         for (int i : new int[]{3, 11}) {
             buttons[3][i].setBackground(DWRed);
             buttons[3][i].setForeground(Color.WHITE);
@@ -168,15 +149,6 @@ public class BoardPanel extends JPanel implements ScrabbleView {
             buttons[3][i].setBorderPainted(false);
         }
 
-        for (int i = 0; i <= 14; i += 7) {
-            buttons[3][i].setBackground(DLBlue);
-            buttons[3][i].setForeground(Color.WHITE);
-            buttons[3][i].setText("DL");
-            buttons[3][i].setOpaque(true);
-            buttons[3][i].setBorderPainted(false);
-        }
-
-        // 4 th Row Grouping
         for (int i : new int[]{4, 10}) {
             buttons[4][i].setBackground(DWRed);
             buttons[4][i].setForeground(Color.WHITE);
@@ -185,68 +157,6 @@ public class BoardPanel extends JPanel implements ScrabbleView {
             buttons[4][i].setBorderPainted(false);
         }
 
-        // 5 th Row Grouping
-        for (int i : new int[]{1, 5, 9, 13, 13}) {
-            buttons[5][i].setBackground(TLBlue);
-            buttons[5][i].setForeground(Color.WHITE);
-            buttons[5][i].setText("TL");
-            buttons[5][i].setOpaque(true);
-            buttons[5][i].setBorderPainted(false);
-        }
-
-        // 6 th Row Grouping
-        for (int i : new int[]{2, 6, 8, 12}) {
-            buttons[6][i].setBackground(DLBlue);
-            buttons[6][i].setForeground(Color.WHITE);
-            buttons[6][i].setText("DL");
-            buttons[6][i].setOpaque(true);
-            buttons[6][i].setBorderPainted(false);
-        }
-
-        // 7 th Row Grouping
-        buttons[7][0].setBackground(TWRed);
-        buttons[7][0].setForeground(Color.WHITE);
-        buttons[7][0].setText("TW");
-        buttons[7][0].setOpaque(true);
-        buttons[7][0].setBorderPainted(false);
-
-        buttons[7][14].setBackground(TWRed);
-        buttons[7][14].setForeground(Color.WHITE);
-        buttons[7][14].setText("TW");
-        buttons[7][14].setOpaque(true);
-        buttons[7][14].setBorderPainted(false);
-
-        buttons[7][4].setBackground(DLBlue);
-        buttons[7][4].setForeground(Color.WHITE);
-        buttons[7][4].setText("DL");
-        buttons[7][4].setOpaque(true);
-        buttons[7][4].setBorderPainted(false);
-
-        buttons[7][11].setBackground(DLBlue);
-        buttons[7][11].setForeground(Color.WHITE);
-        buttons[7][11].setText("DL");
-        buttons[7][11].setOpaque(true);
-        buttons[7][11].setBorderPainted(false);
-
-        // 8 th Row Grouping
-        for (int i : new int[]{2, 6, 8}) {
-            buttons[8][i].setBackground(DLBlue);
-            buttons[8][i].setForeground(Color.WHITE);
-            buttons[8][i].setText("DL");
-            buttons[8][i].setOpaque(true);
-            buttons[8][i].setBorderPainted(false);
-        }
-
-        // 9 th Row Grouping
-        for (int i = 1; i <= 13; i += 4) {
-            buttons[9][i].setBackground(TLBlue);
-            buttons[9][i].setForeground(Color.WHITE);
-            buttons[9][i].setText("TL");
-            buttons[9][i].setOpaque(true);
-            buttons[9][i].setBorderPainted(false);
-        }
-
-        // 10 th Row Grouping
         for (int i : new int[]{10, 4}) {
             buttons[10][i].setBackground(DWRed);
             buttons[10][i].setForeground(Color.WHITE);
@@ -255,7 +165,6 @@ public class BoardPanel extends JPanel implements ScrabbleView {
             buttons[10][i].setBorderPainted(false);
         }
 
-        // 11 th Row Grouping
         for (int i : new int[]{11, 3}) {
             buttons[11][i].setBackground(DWRed);
             buttons[11][i].setForeground(Color.WHITE);
@@ -264,15 +173,6 @@ public class BoardPanel extends JPanel implements ScrabbleView {
             buttons[11][i].setBorderPainted(false);
         }
 
-        for (int i = 0; i <= 14; i += 7) {
-            buttons[11][i].setBackground(DLBlue);
-            buttons[11][i].setForeground(Color.WHITE);
-            buttons[11][i].setText("DL");
-            buttons[11][i].setOpaque(true);
-            buttons[11][i].setBorderPainted(false);
-        }
-
-        // 12 th Row Grouping
         for (int i : new int[]{12, 2}) {
             buttons[12][i].setBackground(DWRed);
             buttons[12][i].setForeground(Color.WHITE);
@@ -281,22 +181,38 @@ public class BoardPanel extends JPanel implements ScrabbleView {
             buttons[12][i].setBorderPainted(false);
         }
 
-        for (int i : new int[]{6, 8}) {
-            buttons[12][i].setBackground(DLBlue);
-            buttons[12][i].setForeground(Color.WHITE);
-            buttons[12][i].setText("DL");
-            buttons[12][i].setOpaque(true);
-            buttons[12][i].setBorderPainted(false);
-        }
-
-
-        // 13 th Row Grouping
         for (int i : new int[]{1, 13}) {
             buttons[13][i].setBackground(DWRed);
             buttons[13][i].setForeground(Color.WHITE);
             buttons[13][i].setText("DW");
             buttons[13][i].setOpaque(true);
             buttons[13][i].setBorderPainted(false);
+        }
+
+
+        //  Triple Letter Blue Extract Method
+        for (int i : new int[]{5, 9}) {
+            buttons[1][i].setBackground(TLBlue);
+            buttons[1][i].setForeground(Color.WHITE);
+            buttons[1][i].setText("TL");
+            buttons[1][i].setOpaque(true);
+            buttons[1][i].setBorderPainted(false);
+        }
+
+        for (int i : new int[]{1, 5, 9, 13, 13}) {
+            buttons[5][i].setBackground(TLBlue);
+            buttons[5][i].setForeground(Color.WHITE);
+            buttons[5][i].setText("TL");
+            buttons[5][i].setOpaque(true);
+            buttons[5][i].setBorderPainted(false);
+        }
+
+        for (int i = 1; i <= 13; i += 4) {
+            buttons[9][i].setBackground(TLBlue);
+            buttons[9][i].setForeground(Color.WHITE);
+            buttons[9][i].setText("TL");
+            buttons[9][i].setOpaque(true);
+            buttons[9][i].setBorderPainted(false);
         }
 
         for (int i : new int[]{5, 9}) {
@@ -307,13 +223,72 @@ public class BoardPanel extends JPanel implements ScrabbleView {
             buttons[13][i].setBorderPainted(false);
         }
 
-        // 14 th Row Grouping
+
+
+
+        //  Double Letter Blue Extract Method
+        for (int i : new int[]{3, 11}) {
+            buttons[0][i].setBackground(DLBlue);
+            buttons[0][i].setForeground(Color.WHITE);
+            buttons[0][i].setText("DL");
+            buttons[0][i].setOpaque(true);
+            buttons[0][i].setBorderPainted(false);
+        }
+
+        for (int i : new int[]{6, 8}) {
+            buttons[2][i].setBackground(DLBlue);
+            buttons[2][i].setForeground(Color.WHITE);
+            buttons[2][i].setText("DL");
+            buttons[2][i].setOpaque(true);
+            buttons[2][i].setBorderPainted(false);
+        }
+
         for (int i = 0; i <= 14; i += 7) {
-            buttons[14][i].setBackground(TWRed);
-            buttons[14][i].setForeground(Color.WHITE);
-            buttons[14][i].setText("TW");
-            buttons[14][i].setOpaque(true);
-            buttons[14][i].setBorderPainted(false);
+            buttons[3][i].setBackground(DLBlue);
+            buttons[3][i].setForeground(Color.WHITE);
+            buttons[3][i].setText("DL");
+            buttons[3][i].setOpaque(true);
+            buttons[3][i].setBorderPainted(false);
+        }
+
+        for (int i : new int[]{2, 6, 8, 12}) {
+            buttons[6][i].setBackground(DLBlue);
+            buttons[6][i].setForeground(Color.WHITE);
+            buttons[6][i].setText("DL");
+            buttons[6][i].setOpaque(true);
+            buttons[6][i].setBorderPainted(false);
+        }
+
+        for (int j : new int[]{4, 11}) {
+            buttons[7][j].setBackground(DLBlue);
+            buttons[7][j].setForeground(Color.WHITE);
+            buttons[7][j].setText("DL");
+            buttons[7][j].setOpaque(true);
+            buttons[7][j].setBorderPainted(false);
+        }
+
+        for (int i : new int[]{2, 6, 8}) {
+            buttons[8][i].setBackground(DLBlue);
+            buttons[8][i].setForeground(Color.WHITE);
+            buttons[8][i].setText("DL");
+            buttons[8][i].setOpaque(true);
+            buttons[8][i].setBorderPainted(false);
+        }
+
+        for (int i = 0; i <= 14; i += 7) {
+            buttons[11][i].setBackground(DLBlue);
+            buttons[11][i].setForeground(Color.WHITE);
+            buttons[11][i].setText("DL");
+            buttons[11][i].setOpaque(true);
+            buttons[11][i].setBorderPainted(false);
+        }
+
+        for (int i : new int[]{6, 8}) {
+            buttons[12][i].setBackground(DLBlue);
+            buttons[12][i].setForeground(Color.WHITE);
+            buttons[12][i].setText("DL");
+            buttons[12][i].setOpaque(true);
+            buttons[12][i].setBorderPainted(false);
         }
 
         for (int i : new int[]{3, 11}) {
