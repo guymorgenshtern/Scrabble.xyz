7d626ad848b4f51929607b8ab5802cdfd4fc05af
Emily Tang <emilytang2002@gmail.com>
2022-11-22 12:56:59 -0500

    changed BoardClick from a record to a class

--- src/BoardClick.java
+++ src/BoardClick.java
@@ -1,15 +1,23 @@
 /**
  * A BoardClick holds a coordinate on the Board, and a Letter to place at that coordinate.
- *
- * @param coords An array of integers to represent a coordinate on the Board.
- * @param letter A String to represent a letter to place at coords on the Board.
  */
-public record BoardClick(int[] coords, String letter) {
+public class BoardClick {
+
+    private final int[] coords;
+    private final String letter;
+
+    /**
+     * @param coords An array of integers to represent a coordinate on the Board.
+     * @param letter A String to represent a letter to place at coords on the Board.
+     */
+    public BoardClick(int[] coords, String letter) {
+        this.coords = coords;
+        this.letter = letter;
+    }
 
     /**
      * @return An array of integers to represent a coordinate on the Board.
      */
-    @Override
     public int[] coords() {
         return coords;
     }
@@ -17,8 +25,8 @@ public record BoardClick(int[] coords, String letter) {
     /**
      * @return A String to represent a letter to place at coords on the Board.
      */
-    @Override
     public String letter() {
         return letter;
     }
+
 }
